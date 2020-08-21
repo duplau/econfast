df51b87fd957afa64db21f4f8c4f3b8be66f1a6d
duplau <fintech_1@mbpdeseuriteadm.home>
2020-08-21 12:10:10 +0200

    More testing

--- test_econfast.py
+++ test_econfast.py
@@ -1,5 +1,6 @@
 import unittest, json
 from index_publis import *
+from index_authors import *
 
 TITLE_0 = u"Labor market and employment in Uzbekistan"
 ABSTRACT_0 = u"The aim of this article is to discuss how to increase employment in different regions. Conclusions drawn are that town halls play key role in regional employment. They should analyze and estimate formed social and demographic situation in the region clarify the share of the employed and unemployed body-able population, the reasons of the formed situation, elaborate and realize the measures on providing rational employment. A special attention should be paid to the existing disproportion between the rates of the new working places growth and the population increase creating definite difficulties for the youth entering the labor market, restraining the latter s migration to the capital, other regions and abroad in searching a job. It is obvious that one shouldn t fail to take into account the measures connected with the motivation and stimulation of labor and enhancement of the social protection of the population as a whole."
@@ -36,6 +37,11 @@ class TestEconFast(unittest.TestCase):
         publis = list(parse_repec_file(path))
         print("Parsed {} publications from {}".format(len(publis), path))
         self.assertEqual(len(publis), 4)
+
+    def test_name_hashing(self):
+      aliases = ["Stiglitz Joseph E", "Stiglitz, Joseph E.", "Joseph E. Stiglitz", "Joesph E. Stiglitz"]
+      hashes = set([hash_name(full_name) for full_name in aliases])
+      self.assertEqual(len(hashes), 1, "Found several hashes: {}".format(hashes))      
 
 if __name__ == '__main__':
     unittest.main()
